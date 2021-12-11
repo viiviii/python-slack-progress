d1e34b77552a86b12db250c7d2f23414ff5857b4
viiviii <viiiix.viiviii@gmail.com>
2021-12-11 21:20:40 +0900

    :sparkles: feature: Page class 추가

---
+++ progress/page.py
@@ -0,0 +1,39 @@
+from typing import Final
+
+
+class Range:
+    def __init__(self, first: int, last: int) -> None:
+        Range._validate(first, last)
+        self.first: Final = first
+        self.last: Final = last
+
+    def __str__(self) -> None:
+        return 'Range({}, {})'.format(self.first, self.last)
+
+    @staticmethod
+    def _validate(first: int, last: int) -> None:
+        if not Range._valid_types(first, last):
+            raise TypeError(f"Arguments is not int type: first={type(first)}, last={type(last)}")
+
+    @staticmethod
+    def _valid_types(first: int, last: int) -> bool:
+        return type(first) is int and type(last) is int
+
+
+class Page:
+    def __init__(self, _all: Range, goal: Range) -> None:
+        Page._validate(_all, goal)
+        self.all: Final = _all
+        self.goal: Final = goal
+
+    def __str__(self) -> None:
+        return 'Page(all={}, goal={})'.format(self.all, self.goal)
+
+    @staticmethod
+    def _validate(_all: Range, goal: Range) -> None:
+        if not Page._valid_ranges(_all, goal):
+            raise ValueError(f"Invalid progress ranges: {_all}, {goal}")
+
+    @staticmethod
+    def _valid_ranges(_all: Range, goal: Range) -> bool:
+        return 0 < _all.first <= goal.first <= goal.last <= _all.last < 3000
