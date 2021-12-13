65f44a2d952d13773ea9f70861fad62c0b4f91b8
viiviii <viiiix.viiviii@gmail.com>
2021-12-13 19:25:16 +0900

    :white_check_mark: test: lambda handler 테스트 추가

---
+++ tests/test_lambda_handler.py
@@ -0,0 +1,18 @@
+from main import decode, parse_query_string, lambda_handler
+
+
+def test_lambda_handler():
+    dummy = {'body': 'dGV4dD05NQ=='}
+    actual = lambda_handler(dummy, None)
+    assert 200 == actual.get("statusCode")
+
+
+def test_decode():
+    actual = decode("dGV4dD05NQ==")
+    assert "text=95" == actual
+
+
+def test_parse_query_string():
+    actual = parse_query_string("text=95")
+    assert dict == type(actual)
+    assert ['95'] == actual.get("text")
